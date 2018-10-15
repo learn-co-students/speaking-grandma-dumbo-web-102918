3737fc595dc755cdd0d5423f13e1ebaa1531fbe0
Terrance Rose Jr <trose618@gmail.com>
2018-10-15 19:41:06 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,11 @@
 
 def
 speak_to_grandma(phrase)
-  if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU     GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  else
-    return "NO, NOT SINCE 1938!"
-  end
+if phrase != phrase.upcase
+  return "HUH?! SPEAK UP, SONNY!"
+elsif phrase == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
+else
+  return "NO, NOT SINCE 1938!"
+end
 end
